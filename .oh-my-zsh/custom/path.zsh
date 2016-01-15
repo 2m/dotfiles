b4fb12c53cad94e32d396c8ed730e0792b5da84f
Martynas Mickevičius <mmartynas@gmail.com>
2016-01-15 06:42:34 +0200

    gopath

--- .oh-my-zsh/custom/path.zsh
+++ .oh-my-zsh/custom/path.zsh
@@ -1,5 +1,5 @@
-RUBY_GEM_BIN=~/.gem/ruby/2.1.0/bin
+RUBY_GEM_BIN=~/.gem/ruby/2.3.0/bin
 LOCAL_BIN=~/.local/bin
 
-PATH=$PATH:$RUBY_GEM_BIN:$LOCAL_BIN
-export PATH
+export PATH=$PATH:$RUBY_GEM_BIN:$LOCAL_BIN
+export GOPATH=~/.local/share/gopath
