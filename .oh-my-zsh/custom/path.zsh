471bb1bf943dcb11df57d9e9e2704ae55ae1558c
Martynas Mickevičius <mmartynas@gmail.com>
2016-03-18 13:12:13 +0200

    move sbt opts to java opts and add multirust bins to path

--- .oh-my-zsh/custom/path.zsh
+++ .oh-my-zsh/custom/path.zsh
@@ -1,5 +1,6 @@
 RUBY_GEM_BIN=~/.gem/ruby/2.3.0/bin
 LOCAL_BIN=~/.local/bin
+CARGO_STABLE_BIN=~/.multirust/toolchains/stable/cargo/bin
 
-export PATH=$PATH:$RUBY_GEM_BIN:$LOCAL_BIN
+export PATH=$PATH:$RUBY_GEM_BIN:$LOCAL_BIN:$CARGO_STABLE_BIN
 export GOPATH=~/.local/share/gopath
