634ed81892d916364be4218f78f291498dcf9d9d
Martynas Mickevicius <mmartynas@gmail.com>
2016-09-13 12:25:45 +0300

    add cargo bin

--- .oh-my-zsh/custom/path.zsh
+++ .oh-my-zsh/custom/path.zsh
@@ -1,6 +1,7 @@
 RUBY_GEM_BIN=~/.gem/ruby/2.3.0/bin
 LOCAL_BIN=~/.local/bin
 CARGO_STABLE_BIN=~/.multirust/toolchains/stable/cargo/bin
+CARGO_BIN=~/.cargo/bin
 
-export PATH=$PATH:$RUBY_GEM_BIN:$LOCAL_BIN:$CARGO_STABLE_BIN
+export PATH=$PATH:$RUBY_GEM_BIN:$LOCAL_BIN:$CARGO_STABLE_BIN:$CARGO_BIN
 export GOPATH=~/.local/share/gopath
