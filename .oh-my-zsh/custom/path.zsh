8567044392e1dd656f27e483e4f0c0b3840e8d3c
Martynas Mickevičius <martynas@2m.lt>
2017-03-15 23:03:04 +0200

    add go get installs to path

--- .oh-my-zsh/custom/path.zsh
+++ .oh-my-zsh/custom/path.zsh
@@ -1,8 +1,10 @@
+export GOPATH=~/.local/share/gopath
+export RUST_SRC_PATH=~/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src/
+
 RUBY_GEM_BIN=~/.gem/ruby/2.3.0/bin
 LOCAL_BIN=~/.local/bin
 CARGO_STABLE_BIN=~/.multirust/toolchains/stable/cargo/bin
 CARGO_BIN=~/.cargo/bin
+GOPATH_BIN=$GOPATH/bin
 
-export PATH=$PATH:$RUBY_GEM_BIN:$LOCAL_BIN:$CARGO_STABLE_BIN:$CARGO_BIN
-export GOPATH=~/.local/share/gopath
-export RUST_SRC_PATH=~/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src/
+export PATH=$PATH:$RUBY_GEM_BIN:$LOCAL_BIN:$CARGO_STABLE_BIN:$CARGO_BIN:$GOPATH_BIN
