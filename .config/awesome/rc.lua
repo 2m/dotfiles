f4562354755654fea222ee013bdf534a70e4d3d5
Martynas Mickevičius <martynas@2m.lt>
2017-04-20 23:03:09 +0300

    disable icons

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -44,6 +44,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(awful.util.get_themes_dir() .. "zenburn/theme.lua")
+beautiful.tasklist_disable_icon = true
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty -e tmux"
