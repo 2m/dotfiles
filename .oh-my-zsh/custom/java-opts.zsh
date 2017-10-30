756dd0212c78e247ddf8aaf5a0be796baa2b0a6e
Martynas Mickevičius <self@2m.lt>
2017-10-30 09:14:17 +0200

    Remove Java < 8 support

--- .oh-my-zsh/custom/java-opts.zsh
+++ .oh-my-zsh/custom/java-opts.zsh
@@ -1,24 +1,15 @@
 function javaOpts() {
 
-  JAVA_VERSION=`java -version 2>&1 | grep "version" | cut -d '"' -f2 | cut -d '.' -f2`
-
   JAVA_OPTS=""
   JAVA_OPTS="$JAVA_OPTS -Xmx3g"
   JAVA_OPTS="$JAVA_OPTS -Xms2g"
   JAVA_OPTS="$JAVA_OPTS -Xss2m"
   JAVA_OPTS="$JAVA_OPTS -XX:ReservedCodeCacheSize=256m" # Maximum size for the JIT's code cache
   JAVA_OPTS="$JAVA_OPTS -XX:+TieredCompilation"         # Increase startup speed with tiered compilation
-  #JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC"        # Use Mark Sweep GC
   JAVA_OPTS="$JAVA_OPTS -XX:+CMSClassUnloadingEnabled"  # GC sweep PermGen
   JAVA_OPTS="$JAVA_OPTS -XX:+UseCompressedOops"         # Allow 32bit references in 64bit VM
   JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/urandom" # Do not block on low entropy
   JAVA_OPTS="$JAVA_OPTS -Dawt.useSystemAAFontSettings=on"      # Fixes font antialiasing
-
-  if [ "$JAVA_VERSION" -lt "8" ]; then
-    JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=512M"
-  else
-    # Default JDK 8 metaspace is unlimited
-  fi
 
   if [[ $1 == "debug" ]] ; then
     JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
