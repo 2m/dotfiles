bb8c836399fbfd490bfb8de1c429fb8d733530b6
Martynas Mickevičius <self@2m.lt>
2018-06-04 22:04:31 +0300

    chroot, no more pacaur and only sbt 1 plugins

--- .oh-my-zsh/custom/fortuna.zsh
+++ .oh-my-zsh/custom/fortuna.zsh
@@ -1,3 +1,2 @@
 if [[ $(basename -s .zsh $0) == $(hostname) ]] ; then
-  alias 'pacaur=env XDG_CACHE_HOME=/data/cache pacaur'
 fi
---
+++ .sbt/1.0/plugins/only_sbt_1.sbt
@@ -0,0 +1,1 @@
+addSbtPlugin("org.scalameta" % "sbt-metals" % "0.1.0-M1+131-42765d7e")
