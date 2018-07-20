647c30884e2777f4f69db17eb8e5a89d3469ce69
Martynas Mickevičius <self@2m.lt>
2018-07-20 19:12:18 +0300

    add fish configuration file

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+fundle plugin 'fisherman/fzf'
+fundle plugin 'oh-my-fish/theme-mtahmed'
+fundle plugin '2m/fish-tmux-gitbar'
+fundle plugin 'ogham/exa' --path 'contrib'
+fundle plugin '2m/fish-exa-aliases'
+
+fundle init
