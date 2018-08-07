d1b9cfd1fbe6071cc876dab0b0e2627ad58604d8
Martynas Mickevičius <self@2m.lt>
2018-08-07 11:17:06 +0300

    GPG TTY plugin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,6 @@ fundle plugin 'oh-my-fish/theme-mtahmed'
 fundle plugin '2m/fish-tmux-gitbar'
 fundle plugin 'ogham/exa' --path 'contrib'
 fundle plugin '2m/fish-exa-aliases'
+fundle plugin '2m/fish-gpg-tty'
 
 fundle init
