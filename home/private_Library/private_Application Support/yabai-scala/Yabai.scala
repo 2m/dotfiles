23eb0270623d60207e671da31e8e63cd495c1c6d
Martynas Mickevičius <self@2m.lt>
2021-04-25 10:20:40 +0300

    yabai configuration using yabai-scala

---
+++ home/private_Library/private_Application Support/yabai-scala/Yabai.scala
@@ -0,0 +1,51 @@
+// 2> /dev/null \
+/*
+scala_version="3.0.0-RC3"
+dependencies="
+  lt.dvim.yabai:yabai-scala_3.0.0-RC3:1.0.2+11-36b48b06-SNAPSHOT
+"
+repositories="
+  sonatype:public
+  sonatype:snapshots
+"
+source $(curl -sL git.io/boot-tsk | sh)
+run
+exit
+ */
+
+import lt.dvim.yabai.*
+
+import scala.language.implicitConversions
+
+@main def run() =
+  for {
+    _ <- (yabai.spaces.max until 10)
+  } yield yabai ! "space --create"
+
+  // Query for app names and title of running apps with:
+  // yabai -m query --windows | jq '.[] | {app: .app, title: .title}'
+  yabai no_manage (Rule.App("choose"), Rule.App("System Preferences"), Rule.Title("gpg-copy"))
+
+  yabai window_topmost on
+  yabai window_gap 2
+  yabai window_shadow off
+
+  yabai layout bsp
+  yabai auto_balance off
+  yabai window_placement second_child
+  yabai split_ratio 0.5
+
+  yabai mouse_follows_focus off
+  yabai focus_follows_mouse autofocus
+
+  yabai mouse_modifier ctrl
+  yabai mouse_action1 resize
+  yabai mouse_action2 move
+
+  yabai padding 5
+
+  yabai window_opacity on
+  yabai active_window_opacity 1.0
+  yabai normal_window_opacity 0.9
+
+  yabai window_border off
